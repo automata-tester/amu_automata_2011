81f07ec13f41e76849a69e76d5a7bb75c088a67a
Filip Gralinski <filipg@ceti.pl>
2011-09-16 13:21:47 +0200

    first version

---
+++ daut-automata-base/main/java/pl/edu/amu/wmi/daut/base/AutomatonSpecification.java
@@ -0,0 +1,9 @@
+
+
+class pl.amu.edu.wmi.daut.base.AutomatonSpecification {
+
+    public abstract Object addState();
+    public abstract Object addTransition(Object from, Object to,
+
+
+};
---
+++ daut-automata-base/main/java/pl/edu/amu/wmi/daut/base/State.java
@@ -0,0 +1,11 @@
+package pl.amu.edu.wmi.daut.base;
+
+/**
+ * Pusty interfejs dla stanów.
+ *
+ * Konkretne klasy reprezentujące poszczególne rodzaje automatów mogą
+ * używać dowolnych klas do reprezentowania stanów.
+ */
+interface State {
+
+}
